bf4a71bd6debbb20bad5d8132814c910f88f51e4
SantiLopBla <slopez40013@ufide.ac.cr>
2025-06-24 22:40:39 -0600

    Add files via upload
    Practica 3, la hice yo, me la dio Chat

---
+++ Practica3/src/practica3/Estado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package practica3;
+
+/**
+ *
+ * @author sanlo
+ */
+public enum Estado {
+    Aprobado,
+    Reprobado,
+    Pendiente,
+}
